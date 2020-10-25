1fb5b2ef38115fbad64fca90ed94e96e680ccf06
Emma Wishart <emma.wishart@twikit.com>
2020-10-25 14:02:45 +0100

    can now display data from db directly to page

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ const express = require('express');
 const BlogPost = require('../models/blogModelAndSchema')
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "Welcome to the application." });
+router.get("/test", (req, res) => {
+    res.json({message: "Welcome to the application."});
 });
 
 router.post('/save', (req, res) => {
@@ -18,20 +18,22 @@ router.post('/save', (req, res) => {
             res.status(500).json({
                 msg: 'There was an internal server error.'
             });
-        } else {
-            res.json({
-                msg: 'Your data has been saved.'
-            })
+            return;
         }
+
+        return res.json({
+            msg: 'Your data has been saved.'
+        });
     })
 
 
 });
 
-router.get('/1', (req, res) => {
+router.get('/', (req, res) => {
     BlogPost.find({})
         .then((data) => {
             console.log('Data:', data);
+            res.json(data);
         })
         .catch((error) => {
             console.log('error:', error)
